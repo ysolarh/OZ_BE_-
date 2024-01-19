a94a721c8086b0c681338658406a0d51bf8f4a8c
ysolarh <ysolarh@gmail.com>
2024-01-19 11:55:21 +0900

    Refactor: remove redundant part
    file_utils 관련 부분 필요 없어서 뺌

--- 7_mini_project_webscraping/fruitsfamily/app.py
+++ 7_mini_project_webscraping/fruitsfamily/app.py
@@ -1,13 +1,12 @@
 from data.crawling import Crawling
 from console.console_writer import ConsoleWriter
 from data.save_to_db import FruitsDB
-# from utils.file_utils import FileUtils
+
 
 class Application:
     def __init__(self):
         self.__running = True
         self.datas = []
-        # FileUtils.set_path()
 
     def run(self):
         if self.__running:

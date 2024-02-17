ce84f1e95e73d58060f98be08b4439a7540feab3
ysolarh <ysolarh@gmail.com>
2024-02-17 21:27:52 +0900

    [django_day3] update config/urls.py

--- 8_Django/django_class/config/urls.py
+++ 8_Django/django_class/config/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('feeds/', include('feeds.urls'))
+    path('api/v1/feeds/', include('feeds.urls')),
+    path('api/v1/users/', include('users.urls')),
 ]

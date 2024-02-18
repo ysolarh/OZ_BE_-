d644abcc9bb35a0ac8ea9e1889e5170c463cc554
ysolarh <ysolarh@gmail.com>
2024-02-18 21:33:53 +0900

    [django_day3] ~26강

--- 8_Django/django_class/feeds/views.py
+++ 8_Django/django_class/feeds/views.py
@@ -1,15 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from .models import Feed
+from .serializers import FeedSerializer
 
 
-# Create your views here.
-def show_feed(request):
-    return HttpResponse("show feed")
+class Feeds(APIView):
+    def get(self, request):
+        feeds = Feed.objects.all()
+        serializer = FeedSerializer(feeds, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        # if request.user.is_authenticated:
+        serializer = FeedSerializer(data=request.data)
+        if serializer.is_valid():
+            feed = serializer.save(user=request.user)
+            serializer = FeedSerializer(feed)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+        # else:
+        #     raise NotAuthenticated
 
 
-def one_feed(request, feed_id, feed_content):
-    return HttpResponse(f"feed id: {feed_id}, {feed_content}")
+class FeedDetail(APIView):
+    def get_object(self, feed_id):
+        try:
+            return Feed.objects.get(id=feed_id)
+        except Feed.DoesNotExist:
+            raise NotFound
 
-
-def all_feed(request):
-    return HttpResponse("all feed")
+    def get(self, request, feed_id):
+        feed = self.get_object(feed_id)
+        serializer = FeedSerializer(feed)
+        return Response(serializer.data)
--- 8_Django/django_class/users/views.py
+++ 8_Django/django_class/users/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound, ParseError
+from .serializers import MyInfoUserSerializer
+from django.contrib.auth.password_validation import validate_password
 
-# Create your views here.
+
+class Users(APIView):
+    def post(self, request):
+        password = request.data.get('password')
+        serializer = MyInfoUserSerializer(data=request.data)
+
+        try:
+            validate_password(password)
+        except:
+            raise ParseError("Invalid password")
+
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
+            serializer = MyInfoUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            raise ParseError(serializer.errors)

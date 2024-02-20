6181c28a9642f33c0a9c54f41c106285e544556c
ysolarh <ysolarh@gmail.com>
2024-02-20 14:30:37 +0900

    [django_day4] Django BasicAuth(Session login)

--- 8_Django/django_class/users/urls.py
+++ 8_Django/django_class/users/urls.py
@@ -5,5 +5,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path("", views.Users.as_view()),
     path("myinfo", views.MyInfo.as_view()),
-    path("getToken", obtain_auth_token)
+    path("getToken", obtain_auth_token),
+    path("login", views.Login.as_view()),
+    path("logout", views.Logout.as_view())
 ]
--- 8_Django/django_class/users/views.py
+++ 8_Django/django_class/users/views.py
@@ -6,6 +6,8 @@ from .serializers import MyInfoUserSerializer
 from django.contrib.auth.password_validation import validate_password
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import authenticate, login, logout
+from rest_framework import status
 
 
 class Users(APIView):
@@ -46,3 +48,36 @@ class MyInfo(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+
+
+class Login(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if not username or not password:
+            raise ParseError()
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+
+# class Logout(APIView):
+#     permission_classes = [IsAuthenticated]
+#
+#     def post(self, request):
+#         print("request", request.header)
+#         logout(request)
+#         return Response(status=status.HTTP_200_OK)
+class Logout(APIView):
+    permission_classes = [IsAuthenticated]  # 실제로 로그인한 유저인지
+
+    def post(self, request):
+        print("header: ", request.headers)
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
